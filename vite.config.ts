bc0730e9468ab4730fd617a02e6528e159af6f6a
Ranum Khan <57311028+Ranumkhan123@users.noreply.github.com>
2025-12-25 15:02:52 +0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './', // relative paths for Netlify
-  plugins: [react()],
-})
